8afdf2c3e8238eb671161f34958d2954abc522b4
Scott Messier <scott@rightscale.com>
2010-01-21 12:30:45 -0800

    updated metadata

--- cookbooks/db_sqlserver/metadata.rb
+++ cookbooks/db_sqlserver/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "support@rightscale.com"
 license          IO.read(File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'LICENSE')))
 description      "Manages a SQL Server instance"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.1"
+version          "0.0.3"
 
 recipe "db_sqlserver::default", "Not yet implemented"
 recipe "db_sqlserver::do_backup", "Backs up all non-system SQL Server databases to a backup directory."
--- cookbooks/web_iis/metadata.rb
+++ cookbooks/web_iis/metadata.rb
@@ -3,11 +3,10 @@ maintainer_email "support@rightscale.com"
 license          IO.read(File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'LICENSE')))
 description      "Manages IIS"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.2"
+version          "0.0.3"
 
 recipe "web_iis::default", "Not yet implemented"
 recipe "web_iis::do_simple_app_deploy", "Deploys all web app from zipped source to wwwroot under IIS"
-recipe "web_iis::update_page", "Update page content and title"
 
 # general
 attribute "web_iis",
@@ -23,14 +22,3 @@ attribute "web_iis/deploy/web_app_src_zips",
   :display_name => "Web App Source Zips Directory",
   :description => "The path to the directory containing one or more web application source .zip file(s).",
   :default => "d:\\datastore\\aspdotnet\\webapps"
-
-# update
-attribute "web_iis/page_title",
-  :display_name => "Page Title",
-  :description => "Page title",
-  :default => "Page Title"
-  
-attribute "web_iis/page_content",
-  :display_name => "Page Content",
-  :description => "Page content",
-  :default => "Page Content"
