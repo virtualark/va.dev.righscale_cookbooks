3dab72a6d1a778542aa125a8aa4061481bc2b1a7
unknown <Ameer@.(none)>
2010-04-11 20:49:11 +1000

    test

--- cookbooks/hello_world/recipes/do_hello_world.rb
+++ cookbooks/hello_world/recipes/do_hello_world.rb
@@ -4,6 +4,6 @@
 powershell "Print Hello World to a text file  c:\temp\hello_world.txt" do
 
   # FIX: avoiding remote_file provider in windows until it is tested.
-  source_file_path = File.expand_path(File.join(File.dirname(__FILE__), '.', 'do_simple_app_deploy.ps1'))
+  source_file_path = File.expand_path(File.join(File.dirname(__FILE__), '.', 'do_hello_world.ps1'))
   source_path(source_file_path)
 end
--- cookbooks/hello_world/recipes/hello_world.ps1
+++
@@ -1,5 +0,0 @@
-new-item c:\temp -force -type directory
-set-content c:\temp\hello_world.txt -value (Date).ToLongDateString()
-add-content c:\temp\hello_world.txt -value (Date).ToLongTimeString()
-
-Write-EventLog -LogName System  -EntryType Information -Source EventLog -EventId 6009 -Message "*************** Hello World from Chef Cook Book ****************"
