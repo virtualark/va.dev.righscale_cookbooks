168604cdcf10698f995b0e8a5242d1a2d2cfd16c
unknown <Ameer@.(none)>
2010-04-12 13:56:33 +1000

    test

--- cookbooks/hello_world/recipes/do_hello_world.ps1
+++ cookbooks/hello_world/recipes/do_hello_world.ps1
@@ -1,5 +1,13 @@
 new-item c:\temp -force -type directory
 set-content c:\temp\hello_world.txt -value (Date).ToLongDateString()
 add-content c:\temp\hello_world.txt -value (Date).ToLongTimeString()
+
+$PARAMETER = $env:MY_PARAMETER
+if ("$PARAMETER" -eq "")
+{
+    Write-Error "PARAMETER environment variable was not set"
+    exit 100
+}
+
 add-content c:\temp\hello_world.txt -value  "my parameter $env:MY_PARAMETER "
 Write-EventLog -LogName System  -EntryType Information -Source EventLog -EventId 6009 -Message "*************** Hello World from Chef Cook Book ****************"
