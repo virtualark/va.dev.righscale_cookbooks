d4c85814621fc996479ea03668261186fc0ea40b
unknown <Ameer@.(none)>
2010-04-12 13:06:15 +1000

    testing parameters

--- cookbooks/hello_world/recipes/do_hello_world.rb
+++ cookbooks/hello_world/recipes/do_hello_world.rb
@@ -4,9 +4,10 @@
 powershell "Print Hello World to a text file  c:\temp\hello_world.txt" do
 
 	chef_attribute = Chef::Node::Attribute.new(
-                      {'MY_PARAMETER' => "Sample parameter passed using chef attributes",
-                      {},
-                      {})
+						{'MY_PARAMETER' => "Sample parameter passed using chef attributes"},
+						{},
+						{}
+					  )
 	parameters(chef_attribute)
 	# FIX: avoiding remote_file provider in windows until it is tested.
 	source_file_path = File.expand_path(File.join(File.dirname(__FILE__), '.', 'do_hello_world.ps1'))
