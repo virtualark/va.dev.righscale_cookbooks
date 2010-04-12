f0b39f6ba616ca31fd811ea7df842e8ad6cc1e80
unknown <Ameer@.(none)>
2010-04-12 13:01:27 +1000

    testing parameters

--- cookbooks/hello_world/recipes/do_hello_world.ps1
+++ cookbooks/hello_world/recipes/do_hello_world.ps1
@@ -1,5 +1,5 @@
 new-item c:\temp -force -type directory
 set-content c:\temp\hello_world.txt -value (Date).ToLongDateString()
 add-content c:\temp\hello_world.txt -value (Date).ToLongTimeString()
-
+add-conetnt c:\temp\hello_world.txt -value  $env:MY_PARAMETER 
 Write-EventLog -LogName System  -EntryType Information -Source EventLog -EventId 6009 -Message "*************** Hello World from Chef Cook Book ****************"
