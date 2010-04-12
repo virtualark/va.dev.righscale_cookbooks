bfade95c1c22678aac0ec872637c89b407f3ef03
unknown <Ameer@.(none)>
2010-04-12 14:46:38 +1000

    testing parameters

--- cookbooks/hello_world/recipes/do_hello_world.ps1
+++ cookbooks/hello_world/recipes/do_hello_world.ps1
@@ -3,11 +3,11 @@ set-content c:\temp\hello_world.txt -value (Date).ToLongDateString()
 add-content c:\temp\hello_world.txt -value (Date).ToLongTimeString()
 
 $PARAMETER = $env:MY_PARAMETER
-if ("$PARAMETER" -eq "")
-{
-    Write-Error "PARAMETER environment variable was not set"
-    exit 100
-}
+#if ("$PARAMETER" -eq "")
+#{
+    #Write-Error "PARAMETER environment variable was not set"
+    #exit 100
+#}
 
 add-content c:\temp\hello_world.txt -value  "my parameter $env:MY_PARAMETER "
 Write-EventLog -LogName System  -EntryType Information -Source EventLog -EventId 6009 -Message "*************** Hello World from Chef Cook Book ****************"
