c7908059f0ed9a4a793bb8e15c85ee8d18e35b2b
unknown <Ameer@.(none)>
2010-04-12 13:39:27 +1000

    testing attributes

--- cookbooks/hello_world/recipes/do_hello_world.rb
+++ cookbooks/hello_world/recipes/do_hello_world.rb
@@ -4,11 +4,9 @@
 powershell "Print Hello World to a text file  c:\temp\hello_world.txt" do
 
 
-	#test_string = @node[:hello_world][:test_string]
-	#parameters('MY_PARAMETER' => test_string)
-  
-	web_app_src_zips = @node[:web_iis][:deploy][:web_app_src_zips]
-	parameters('MY_PARAMETER' => web_app_src_zips)
+	test_string = @node[:test_string]
+	parameters('MY_PARAMETER' => test_string)
+
   
 	# FIX: avoiding remote_file provider in windows until it is tested.
 	source_file_path = File.expand_path(File.join(File.dirname(__FILE__), '.', 'do_hello_world.ps1'))
