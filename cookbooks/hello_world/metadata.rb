3f4c8724a72500b5c36e967404b4708fb8c7d37b
unknown <Ameer@.(none)>
2010-04-12 13:31:44 +1000

    added attributes

--- cookbooks/hello_world/metadata.rb
+++ cookbooks/hello_world/metadata.rb
@@ -6,3 +6,9 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
 
 recipe "hello_world::do_hello_world", "Prints hellow world to a text file c:\temp\hello_world.txt and to the eventviewer"
+
+attribute "hello_world/test_string",
+  :display_name => "Passes sample string to hello world script",
+  :description => "Passes sample string to hello world script",
+  :default => "Hello World",
+  :recipes => ["hello_world::do_hello_world"]
